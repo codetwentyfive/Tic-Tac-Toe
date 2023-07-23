ae67fd222b265b4cf18a3c3068e4e6557a0201a6
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-07-24 00:04:27 +0200

    set up gameBoard

--- script.js
+++ script.js
@@ -8,12 +8,41 @@ const player = (name, symbol) => {
 
 //factory function to create game board
 const createGameBoard = () => {
-    const board= Array(9).fill('');
+    const board = Array(9).fill('');
 
-    const clearBoard= ()=>{
-        board= Array(9).fill('');
+    const getBoard = () => {
+        return (board);
+    }
+
+    const clearBoard = () => {
+        board = Array(9).fill('');
     };
 
+    const isCellEmpty = (index) => {
+        board[index] === '';
+
+    };
+
+    const placeSymbol = (index, symbol) => {
+        if (isCellEmpty(index) == '') {   //checks if the index is empty and populates it with the symbol
+            board[index] = symbol;
+            return true
+        } else {
+            return false;
+
+        }
+
+
+
+    };
+
+
+    return {
+        getBoard,
+        clearBoard,
+        isCellEmpty,
+        placeSymbol
+    }
 }
 
 createGameBoard();
