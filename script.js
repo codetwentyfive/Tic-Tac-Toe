8062868d0dcd0b0c1008190959c2cefd58a47ba8
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-07-23 23:54:18 +0200

    created bones of the project

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // function to create player
-const player = (name,symbol)=>{
-return{name,symbol};
+const player = (name, symbol) => {
+    return { name, symbol };
+
+}
+
+
+
+//factory function to create game board
+const createGameBoard = () => {
+    const board= Array(9).fill('');
+
+    const clearBoard= ()=>{
+        board= Array(9).fill('');
+    };
+
+}
+
+createGameBoard();
+//create game logic(game controls)
+
+const createGameLogic = () => {
+
 }
