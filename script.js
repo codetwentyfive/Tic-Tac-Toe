496f0b8e1b7de8388c1c0c1b63769dc4e8193f08
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-07-25 03:26:19 +0200

    addded game logic for win conditions and switch turns

--- script.js
+++ script.js
@@ -46,8 +46,58 @@ const createGameBoard = () => {
 }
 
 createGameBoard();
-//create game logic(game controls)
 
+
+//create game logic(game controls)
 const createGameLogic = () => {
 
-}
+    let currentPlayer = player1;
+    let gameBoard = createGameBoard();
+    const boardEl = document.getElementsByClassName(".board");
+
+
+
+    const displayBoard = () => {
+        const board = gameBoard.getBoard();
+        boardEl.innerHTML = '';
+        board.forEach((cell, index) => {
+            const cellEl = document.createElement('div');
+            cellEl.classList.add("cell");
+            cellEl.textContent = cell;
+            cellEl.addEventListener('click', () => handleCellClick(index));
+            boardEl.appendChild(cellEl);
+        });
+
+    };
+
+    const switchTurn = () => {
+        // math bby x = (x === y) ? z : y
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+
+    const checkWin = (symbol) => {
+        const board = gameBoard.getBoard();
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        return winPatterns.some(pattern => pattern.every(index => board[index] === symbol));
+
+    };
+
+
+
+
+
+};
+
+
+
+//create players
+const player1 = player("player1", "X");
+const player2 = player("player2", "O");
+
+console.log("Hello")
